fe215ac1fa8fde709d5f9abc78ef01c38bf054b0
Jaime Rosado <jaime_ros@fis.ucm.es>
2023-02-08 14:36:33 +0100

    Borro atomictools_modificado

--- notebooks/atomictools_modificado/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-from .radial_functions import R_hydrog, R_num, radial
-from .spherical_harmonics import spherical_harmonic, ftheta, fphi_real, fphi_complex
-from .orbitals import orbital_hydrog
