01f9caf271cfe290a8efe203f064d46f6f25f895
Jaime Rosado <jaime_ros@fis.ucm.es>
2023-02-08 14:45:07 +0100

    Actualizo orbitals.py
    Introduzco parámetro s para la orientación del orbital en el caso de funciones angulares reales. Para s=None, usa un armónico esférico como antes
    
    Redefino theta_dist teniendo en cuenta la renormalización de ftheta
    
    Creo phi_dist para el caso de funciones angulares reales con la distribución en phi
    
    Creo método get_phi que obtiene phi aleatorios según phi_dist
    
    Uso get_hi en el método plot_scatter

--- atomictools/orbitals.py
+++ atomictools/orbitals.py
@@ -21,46 +21,63 @@ def cart_to_sph(x, y, z):
     return r, theta, phi
 
 class orbital_hydrog():
-    def __init__(self, n, l, m, Z=1, mu=1.):
+    def __init__(self, n, l, m, s=None, Z=1, mu=1.):
         self.n = n
         self.l = l
         self.m = m
         self.Z = Z
         self.mu = mu
+        self.s = s
         
         self.R = at.R_hydrog(n, l, Z, mu)
-        self.Y = at.spherical_harmonic(l, m)
+        if s is None:
+            self.Y = at.spherical_harmonic(l, m)
+        else:
+            self.Y = at.real_ang_function(l, m, s)
         
         rmax = self.R.rmax
         self.rmax = rmax
         self.r = self.R.r
-        self.r_dist = self.R.P2.cumsum()*rmax / (self.R.npt-1)
+        self.r_dist = self.R.P2.cumsum() * rmax / (self.R.npt-1)
         self.r_dist /= self.r_dist[-1]
         
         self.theta = self.Y.theta
-        self.theta_dist = (self.Y.ftheta_lm**2 * np.sin(self.theta)).cumsum() * (2.*np.pi**2/49.)
+        self.theta_dist = (self.Y.ftheta_lm**2 * np.sin(self.theta)).cumsum() * np.pi/49.
         self.theta_dist /= self.theta_dist[-1]
+        
+        self.phi = self.Y.phi
+        if s is not None:
+            self.phi_dist = (self.Y.fphi_m**2).cumsum() * 2. * np.pi / 99.
+            self.phi_dist /= self.phi_dist[-1]
         
         self.x, self.y, self.z = np.mgrid[-rmax:rmax:40j, -rmax:rmax:40j, -rmax:rmax:40j]
         r, theta, phi = cart_to_sph(self.x, self.y, self.z)
 
         theta_lm = at.ftheta(l, m, theta)
-        phi_m = at.fphi(m, phi)
+        phi_m = at.fphi(m, phi, s)
+
         R_nl = at.radial(r, n, l, Z, mu)
 
         self.prob = np.abs(theta_lm*phi_m)**2 * R_nl**2
 
     def get_r(self, points=1):
-        #plt.scatter(self.r, self.r_dist)
         p = np.random.random(points)
         r = np.interp(p, self.r_dist, self.r)
         return r
     
     def get_theta(self, points=1):
-        #plt.scatter(self.theta, self.theta_dist)
         p = np.random.random(points)
         theta = np.interp(p, self.theta_dist, self.theta)
         return theta
+    
+    def get_phi(self, points=1):
+        if self.s is None:
+            phi = np.random.random(points)*2.*np.pi
+            return phi
+        else:
+            p = np.random.random(points)
+            phi = np.interp(p, self.phi_dist, self.phi)
+            return phi
         
     def plot_volume(self):
         
@@ -87,7 +104,7 @@ class orbital_hydrog():
         points = int(points)
         r = self.get_r(points)
         theta = self.get_theta(points)
-        phi = np.random.random(points)*2.*np.pi
+        phi = self.get_phi(points)
         rmax = self.rmax
         
         x, y, z = sph_to_cart(r, theta, phi)
